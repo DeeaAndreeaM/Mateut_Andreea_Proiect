2067d50dad8b3f643dbcbf7b62a1edb787d6f706
Mateut Andreea Alina <mateutandreea96@gmail.com>
2022-01-19 21:20:28 +0200

    Proiect Update

--- Mateut_Andreea_Proiect/Pages/Categories/Index.cshtml.cs
+++ Mateut_Andreea_Proiect/Pages/Categories/Index.cshtml.cs
@@ -16,33 +16,41 @@ namespace Mateut_Andreea_Proiect.Pages.Categories
 
         public IndexModel(Mateut_Andreea_Proiect.Data.Mateut_Andreea_ProiectContext context)
         {
+            this.Category = new List<Category>();
             _context = context;
+
         }
 
         public IList<Category> Category { get;set; }
+
         public MouvieData MouvieD { get; set; }
         public int MouvieID { get; set; }
         public int CategoryID { get; set; }
 
-        public async Task OnGetAsync(int? id, int? categoryID)
+        public async Task OnGetAsync()
         {
-            MouvieD = new MouvieData();
-
-            MouvieD.Mouvies = await _context.Mouvie
-            .Include(b => b.Releaser)
-            .Include(b => b.MouvieCategories)
-            .ThenInclude(b => b.Category)
-            .AsNoTracking()
-            .OrderBy(b => b.Title)
-            .ToListAsync();
-            if (id != null)
-            {
-                MouvieID = id.Value;
-                Mouvie mouvie = MouvieD.Mouvies
-                .Where(i => i.ID == id.Value).Single();
-                MouvieD.Categories = mouvie.MouvieCategories.Select(s => s.Category);
-            }
+            Category = await _context.Category.ToListAsync();
         }
+
+        /* public async Task OnGetAsync(int? id, int? categoryID)
+         {
+             MouvieD = new MouvieData();
+
+             MouvieD.Mouvies = await _context.Mouvie
+             .Include(b => b.Releaser)
+             .Include(b => b.MouvieCategories)
+             .ThenInclude(b => b.Category)
+             .AsNoTracking()
+             .OrderBy(b => b.Title)
+             .ToListAsync();
+             if (id != null)
+             {
+                 MouvieID = id.Value;
+                 Mouvie mouvie = MouvieD.Mouvies
+                 .Where(i => i.ID == id.Value).Single();
+                 MouvieD.Categories = mouvie.MouvieCategories.Select(s => s.Category);
+             }
+         }*/
     }
 }
 
